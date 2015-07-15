503e5f01f20de36b2925872900285b803df9818f
Andre Kapp <kappaj@gmail.com>
2015-07-15 12:55:19 +0200

    add some code

--- src/main/java/za/co/celbux/firstGoogleApp/servlets/ModuleInfoDumper.java
+++ src/main/java/za/co/celbux/firstGoogleApp/servlets/ModuleInfoDumper.java
@@ -12,11 +12,11 @@ public class ModuleInfoDumper {
 		// Get the module name handling the current request.
 		String currentModuleName = modulesApi.getCurrentModule();
 		// Get the instanceID handling the current request.
-		String currentInstanceId = modulesApi.getCurrentInstanceId();
+		//String currentInstanceId = modulesApi.getCurrentInstanceId();
 
 		String currentVersion = modulesApi.getCurrentVersion();
 		System.out.println("currentModuleName	=>" + currentModuleName);
-		System.out.println("currentInstanceId	=>" + currentInstanceId);
+		//System.out.println("currentInstanceId	=>" + currentInstanceId);
 		System.out.println("currentVersion		=>" + currentVersion);
 	}
 }
