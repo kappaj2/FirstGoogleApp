b362b90d6a5cb6da76680d4c541188b8c9f2d98d
Andre Kapp <kappaj@gmail.com>
2015-07-15 12:53:10 +0200

    add some code

---
+++ src/main/java/za/co/celbux/firstGoogleApp/servlets/ModuleInfoDumper.java
@@ -0,0 +1,22 @@
+package za.co.celbux.firstGoogleApp.servlets;
+
+import com.google.appengine.api.modules.ModulesService;
+import com.google.appengine.api.modules.ModulesServiceFactory;
+
+public class ModuleInfoDumper {
+
+	public void dumpInfo() {
+
+		ModulesService modulesApi = ModulesServiceFactory.getModulesService();
+
+		// Get the module name handling the current request.
+		String currentModuleName = modulesApi.getCurrentModule();
+		// Get the instanceID handling the current request.
+		String currentInstanceId = modulesApi.getCurrentInstanceId();
+
+		String currentVersion = modulesApi.getCurrentVersion();
+		System.out.println("currentModuleName	=>" + currentModuleName);
+		System.out.println("currentInstanceId	=>" + currentInstanceId);
+		System.out.println("currentVersion		=>" + currentVersion);
+	}
+}
--- src/main/java/za/co/celbux/firstGoogleApp/servlets/TestServlet1.java
+++ src/main/java/za/co/celbux/firstGoogleApp/servlets/TestServlet1.java
@@ -11,27 +11,34 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class TestServlet1 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public TestServlet1() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	private ModuleInfoDumper moduleDumper = new ModuleInfoDumper();
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/plain");
-		response.getWriter().println("Received doGet");
+	public TestServlet1() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/plain");
+		response.getWriter().println("Received doGet");
+		moduleDumper.dumpInfo();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.setContentType("text/plain");
 		response.getWriter().println("Received doPost");
