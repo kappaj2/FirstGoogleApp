9b2a7de167b432cc6ce2dd8c0dda26d4af0d2ed0
Andre Kapp <kappaj@gmail.com>
2015-07-15 13:31:16 +0200

    added unit test

---
+++ src/test/java/za/co/celbux/Test1.java
@@ -0,0 +1,35 @@
+package za.co.celbux;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class Test1 {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@org.junit.Test
+	public void test() {
+		System.out.println("Running unit tests !! *************************************");
+		assertEquals("This should be the same...", 1, 1);
+	}
+
+}
